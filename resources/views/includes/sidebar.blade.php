620901fb7b3301058e0d94bd591c8d1ea2bee86f
Simone Basini <Simone Basini>
2017-01-17 02:36:49 +0100

    debug

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -7,7 +7,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="{{ asset('dist/img') }}/{{ Auth::User()->avatar }}" class="img-circle" alt="User Image">
+          <img src="{{ asset('dist/img') }}/avatar04.png" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>{{ Auth::User()->name }} ({{ Auth::user()->number }})</p>
